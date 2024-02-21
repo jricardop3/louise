bd68cb80d1b3314086bdce3b8a9e6ea3081e7817
Jose Ricardo Peixoto <120475384+jricardop3@users.noreply.github.com>
2024-02-21 09:16:11 -0300

    Initial commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Page Title' }}</title>
+@livewireStyles
+</head>
+<body>
+    <div class="container">
+        {{ $slot }}
+    </div>
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+
+    <div class="bg-primary">
+        <h1>{{ $count }}</h1>
+     
+        <button wire:click="increment">+</button>
+     
+        <button wire:click="decrement">-</button>
+    </div>
